88900ad9e3fc2e4f99b6429f7f9f226b0d14b595
Dennis Owusu <57267272+hale-Dennis@users.noreply.github.com>
2024-04-09 15:48:13 +0000

    form validation

---
+++ spring-boot-validation/src/main/java/com/example/springbootvalidation/model/User.java
@@ -0,0 +1,39 @@
+package com.example.springbootvalidation.model;
+
+public class User {
+    @NotBlank(message = "username cannot be empty")
+    private String name;
+    @NotBlank(message = "password cannot be empty")
+    private String password;
+    private Double grade;
+
+    public User(String name, String password, Double grade) {
+        this.name = name;
+        this.password = password;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Double grade) {
+        this.grade = grade;
+    }
+}
